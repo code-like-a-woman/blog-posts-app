73c6550381e9949f66a1de211fae32df132a4309
Mirna De Jesus <mdjc@users.noreply.github.com>
2025-04-16 14:09:53 -0400

    Set up CI workflow with GitHub Actions (#1)
    * Workflow set up
    * Configure the WorkFlow to build the project and run the tests
    * Fix tests
    * Have a more descriptive name for the job
    * Use GitHub secrets
    * update workflow to run on the main branch
    * update workflow title and README

--- src/main/java/com/example/demo/auth/JwtAuthFilter.java
+++ src/main/java/com/example/demo/auth/JwtAuthFilter.java
@@ -23,7 +23,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain)
             throws ServletException, IOException {
-
+      
         String authHeader = request.getHeader("Authorization");
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String token = authHeader.substring(7);
